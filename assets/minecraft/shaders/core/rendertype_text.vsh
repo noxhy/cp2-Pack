3b2e9984c8f7fcd79fe4c0ec6af8c2cc5780d29a
noxhy <58011428+noxhy@users.noreply.github.com>
2026-03-14 23:38:21 -0400

    reasonless crosshairs

--- assets/minecraft/shaders/core/rendertype_text.vsh
+++ assets/minecraft/shaders/core/rendertype_text.vsh
@@ -26,27 +26,6 @@ out vec4 lightColor;
 #moj_import <spheya_packs_impl.glsl>
 #moj_import <crosshair.glsl>
 
-float getCameraYaw(mat4 viewMat) {
-    vec4 left = vec4(1.0, 0.0, 0.0, 0.0) * viewMat;
-    return atan(left.z, left.x) + radians(180.0);
-}
-
-vec2 getRelativePosition(vec3 position, mat4 viewMat) {
-    return (inverse(viewMat) * vec4((viewMat * vec4(position, 1.0)).xyz, 0.0)).xz;
-}
-
-mat2 createRotationMat(float theta) {
-    return mat2(cos(theta), -sin(theta), sin(theta), cos(theta));
-}
-
-int getGuiScale(mat4 ProjMat, vec2 ScreenSize) {
-    return int(round(ScreenSize.x * ProjMat[0][0] / 2));
-}
-
-float getScreenFOV(mat4 ProjMat) {
-    return atan(1.0, ProjMat[1][1]) * 114.591559;
-}
-
 void main() {
     gl_Position = ProjMat * ModelViewMat * vec4(Position, 1.0);
 
@@ -58,56 +37,6 @@ void main() {
     vertexColor = baseColor * lightColor;
     texCoord0 = UV0;
 
+    if(cp2_apply_crosshair()) return;
     if(applySpheyaPacks()) return;
-
-    // Stealing Code from Reasonless
-    if ((Color.r >= 69. / 255. && Color.r <= 79. / 255.) && (Color.g == 77. / 255.)) {
-        int blueChannel = int(Color.b * 255);
-        bool isIcon = true;
-        int offset = TEXTURE_SIZE / 2;
-        float zPosition = -0.995;
-
-        vec2 vertexPosition;
-        // Calculate necessary information for the minimap.
-        vec2 pixelSize = 2.0 / ScreenSize;
-        vec2 position = getRelativePosition(Position, ModelViewMat);
-        mat2 rotation = createRotationMat(getCameraYaw(ModelViewMat));
-
-        switch (gl_VertexID % 4) {
-            case 0: vertexPosition = vec2(-offset, offset); break;
-            case 1: vertexPosition = vec2(-offset, -offset); break;
-            case 2: vertexPosition = vec2(offset, -offset); break;
-            case 3: vertexPosition = vec2(offset, offset); break;
-        }
-
-        vertexPosition *= CROSSHAIR_SCALE;
-        vertexPosition += vec2(-position.x, position.y) * rotation;
-
-        vertexPosition += vec2((ScreenSize.x / 2.), -(ScreenSize.y / 2.));
-
-        int gap = int(ScreenSize.x / getScreenFOV(ProjMat));
-
-        if (Color.r == 74. / 255.) {
-            vertexPosition.x -= (blueChannel * gap);
-        }
-
-        if (Color.r == 75. / 255.) {
-            vertexPosition.x += (blueChannel * gap);
-        }
-
-        if (Color.r == 76. / 255.) {
-            vertexPosition.y += (blueChannel * gap);
-        }
-
-        if (Color.r == 77. / 255.) {
-            vertexPosition.y -= (blueChannel * gap);
-        }
-
-        vertexPosition *= pixelSize;
-
-        gl_Position = vec4(vertexPosition.x - 1, vertexPosition.y + 1, zPosition, 1.);
-        vertexColor = vec4(vec3(1.), 1.);
-    }
-
-    
 }
--- assets/minecraft/shaders/include/crosshair.glsl
+++ assets/minecraft/shaders/include/crosshair.glsl
@@ -1,5 +1,112 @@
-// Scaling factor for icons displayed on the minimap.
-#define CROSSHAIR_SCALE 2
+bool cp2_apply_crosshair() {
+    #define CROSSHAIR_SIZE 8
+    #define CROSSHAIR_CORNER int(gl_VertexID % 4)
+    #define CROSSHAIR_CORNER_TL 0
+    #define CROSSHAIR_CORNER_BL 1
+    #define CROSSHAIR_CORNER_BR 2
+    #define CROSSHAIR_CORNER_TR 3
+    #define CROSSHAIR_SIDE_TOP 1
+    #define CROSSHAIR_SIDE_RIGHT 2
+    #define CROSSHAIR_SIDE_BOTTOM 3
+    #define CROSSHAIR_SIDE_LEFT 4
 
-// The side length of the background texture in pixels
-#define TEXTURE_SIZE 15
+    #define CROSSHAIR_MAX_HORIZONTAL 16.0
+    #define CROSSHAIR_MAX_VERTICAL 64.0
+    #define CROSSHAIR_MAX_SPREAD 256.0
+
+    // Discard if the foreground color is #637063 (cpc)
+    if (Color.r == (99.0 / 255.0) &&
+        Color.g == (112.0 / 255.0) &&
+        Color.b == (99.0 / 255.0) &&
+        Color.a == (255.0 / 255.0)) {
+        vertexColor = vec4(0.0);
+        return true;
+    }
+
+    vec4 cornerPixel = texture(Sampler0, UV0) * 255.0;
+    // Checks for #637032 (cp2 - feel free to change it)
+    if (cornerPixel.r != 99.0 || 
+        cornerPixel.g != 112.0 ||
+        cornerPixel.b != 50.0 || 
+        cornerPixel.a == 255.0) {
+        return false;
+    }
+
+    vec2 screenPixel = 2.0 / ScreenSize;
+    vec2 texturePixel = 1.0 / textureSize(Sampler0, 0);
+
+    gl_Position = vec4(
+        -1 + (floor(ScreenSize.x / 2) * screenPixel.x), 
+        -1 + (floor(ScreenSize.y / 2) * screenPixel.y), 
+        -0.999, 
+        1.0
+    );
+    
+    switch (CROSSHAIR_CORNER) {
+        case CROSSHAIR_CORNER_TL:
+            gl_Position.x -= screenPixel.x * (CROSSHAIR_SIZE / 2);
+            gl_Position.y += screenPixel.y * (CROSSHAIR_SIZE / 2);
+            texCoord0 += texturePixel * vec2(1, 1);
+            break;
+        case CROSSHAIR_CORNER_BL:
+            gl_Position.x -= screenPixel.x * (CROSSHAIR_SIZE / 2);
+            gl_Position.y -= screenPixel.y * (CROSSHAIR_SIZE / 2);
+            texCoord0 += texturePixel * vec2(1, -1); 
+            break;
+        case CROSSHAIR_CORNER_BR: 
+            gl_Position.x += screenPixel.x * (CROSSHAIR_SIZE / 2);
+            gl_Position.y -= screenPixel.y * (CROSSHAIR_SIZE / 2);
+            texCoord0 += texturePixel * vec2(-1, -1); 
+            break;
+        case CROSSHAIR_CORNER_TR: 
+            gl_Position.x += screenPixel.x * (CROSSHAIR_SIZE / 2);
+            gl_Position.y += screenPixel.y * (CROSSHAIR_SIZE / 2);
+            texCoord0 += texturePixel * vec2(-1, 1); 
+            break;
+    }
+    
+    // Extract dynamic data from color
+    float spread = radians(Color.r * CROSSHAIR_MAX_SPREAD);
+    float hAngle = radians((Color.g * CROSSHAIR_MAX_HORIZONTAL * 2) - CROSSHAIR_MAX_HORIZONTAL);
+    float vAngle = radians(Color.b * CROSSHAIR_MAX_VERTICAL);
+    int zIndex = int(cornerPixel.a) & 15;
+    int colorIndex = int(cornerPixel.a) >> 4;
+
+    // Calculate screen offset
+    float verticalFov = atan(1.0, ProjMat[1][1]) * 2.0;
+    float horizontalFov = atan(1.0, ProjMat[0][0]) * 2.0;
+    float horizontalScale = tan(horizontalFov * 0.5);
+    float verticalScale = tan(verticalFov * 0.5);
+    float offsetX = tan(spread) / horizontalScale;
+    float offsetY = offsetX * float(ScreenSize.x / ScreenSize.y);
+
+    // Offset the crosshair based on spread, angle and zIndex
+    int crosshairSide = int(round(cornerPixel.a / 10.0));
+    switch (crosshairSide) {
+        case CROSSHAIR_SIDE_TOP:
+            gl_Position.y += offsetY;
+            break;
+        case CROSSHAIR_SIDE_BOTTOM:
+            gl_Position.y -= offsetY;
+            break;
+        case CROSSHAIR_SIDE_LEFT:
+            gl_Position.x -= offsetX;
+            break;
+        case CROSSHAIR_SIDE_RIGHT:
+            gl_Position.x += offsetX;
+            break;
+    }
+    gl_Position.x += tan(hAngle) / horizontalScale;
+    gl_Position.y -= tan(vAngle) / verticalScale;
+    gl_Position.z += 0.0001 * float(zIndex);
+
+    // Apply the color from a predefined palette (4-bit)
+    vec3 elementColor = vec3(255.0);
+    switch (colorIndex) {
+        case 0: 
+            elementColor = vec3(255.0, 255.0, 255.0);
+            break;
+    }
+    vertexColor = vec4(elementColor / 255.0, 1.0);
+    return true;
+}
